8ce98feec238c8e5aa24f8684f19e934985c59de
Дмитрий <specx@bk.ru>
2023-05-12 09:34:12 +0300

    dz2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+ if(num1>num2)
+ {
+
+    if(num1>num3)
+ {
+    Console.WriteLine($" число 1(Значение {num1}) больше всех");
+  }  
+ }
+ else if
+ (num2>num3) 
+Console.WriteLine($"число 3(Значение {num3}) больше всех");
+ else {
+  Console.WriteLine($"число 3(Значение {num3}) больше всех");
+ }
+ //
